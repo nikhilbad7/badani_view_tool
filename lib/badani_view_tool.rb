8bb15c667b17744209ac04190072cc9f9956893d
nikbad7 <nikhil.badani@gmail.com>
2018-03-17 03:10:01 +0530

    nikhilviewtool initial commit

---
+++ lib/badani_view_tool.rb
@@ -0,0 +1,6 @@
+require "badani_view_tool/version"
+require "badani_view_tool/renderer"
+
+module BadaniViewTool
+  # Your code goes here...
+end
---
+++ lib/badani_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BadaniViewTool
+  VERSION = "0.1.0"
+end
